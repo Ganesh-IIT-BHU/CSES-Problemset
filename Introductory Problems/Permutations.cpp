ecca3a361393982e5cbae2a984c8603cd10a0d7a
Ganesh-IIT-BHU <57870864+Ganesh-IIT-BHU@users.noreply.github.com>
2021-08-06 17:43:19 +0530

    Add files via upload

---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,85 @@
+/*
+    CSES gannii —  
+    Link to this code: https://cses.fi/paste/c1f0a1abd3aeb95725aa14/
+*/
+
+#include "bits/stdc++.h"
+using namespace std;
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+ 
+template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a){in>>a.fr>>a.sc;return in;}
+template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a){out<<a.fr<<" "<<a.sc;return out;}
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+ 
+const long long INF=1e18;
+const int32_t M=1e9+7;
+const int32_t MM=998244353;
+ 
+ 
+    
+ 
+void solve(){
+ 
+    int n;
+    cin >> n;
+ 
+    int n1 = n;
+    n1--;
+ 
+    if(n==2 || n==3){
+        cout <<  "NO SOLUTION\n";
+        return;
+    }
+ 
+ 
+    while(n1>0){
+ 
+        cout << n1 << " ";
+ 
+        n1 = n1-2;
+    }
+ 
+    while(n>0){
+ 
+        cout << n << " ";
+ 
+        n = n-2;
+    }
+ 
+}
+ 
+ 
+ 
+ 
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #ifdef SIEVE
+        sieve();
+    #endif
+    #ifdef NCR
+        init();
+    #endif
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
